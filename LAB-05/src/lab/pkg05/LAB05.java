f854944a5b8ccc4213af7c502b26836b9b306eb7
Daniel-bermudez137 <123768989+Daniel-bermudez137@users.noreply.github.com>
2023-05-05 15:34:49 -0500

    Add files via upload

---
+++ LAB-05/src/lab/pkg05/LAB05.java
@@ -0,0 +1,64 @@
+mport java.awt.Color;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.chart.ChartUtilities;
+import java.io.File;
+
+public class LeerArchivo {
+    public static void main(String[] args) {
+        String archivo = "nacimientos.csv";
+        String linea;
+        Map<String, Integer> nacimientosPorDepartamento = new HashMap<>();
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            while ((linea = br.readLine()) != null) {
+                String[] elementos = linea.split(",");
+                String departamento = elementos[0].trim();
+                if (nacimientosPorDepartamento.containsKey(departamento)) {
+                    nacimientosPorDepartamento.put(departamento, nacimientosPorDepartamento.get(departamento) + 1);
+                } else {
+                    nacimientosPorDepartamento.put(departamento, 1);
+                }
+            }
+            
+            DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+            for (String departamento : nacimientosPorDepartamento.keySet()) {
+                dataset.addValue(nacimientosPorDepartamento.get(departamento), "Nacimientos", departamento);
+            }
+            
+            JFreeChart chart = ChartFactory.createBarChart(
+                    "Nacimientos por Departamento", 
+                    "Departamento", 
+                    "Cantidad", 
+                    dataset, 
+                    PlotOrientation.VERTICAL, 
+                    false, 
+                    true, 
+                    false);
+            
+            chart.setBackgroundPaint(Color.white);
+            chart.getTitle().setPaint(Color.black);
+            
+            ChartFrame frame = new ChartFrame("Gráfico", chart);
+            frame.setVisible(true);
+            frame.setSize(800, 600);
+            
+            // Definir ruta y nombre del archivo de la gráfica
+            String archivoImagen = "nacimientos_por_departamento.jpeg";
+            
+            // Guardar la gráfica en formato JPEG
+            ChartUtilities.saveChartAsJPEG(new File(archivoImagen), chart, 800, 600);
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo");
+            e.printStackTrace();
+        }
+    }
+}
